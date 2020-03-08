3cc38e4dbfd4756848d226a9ce7fdd9352a72b3c
della-padula <della.kimko@gmail.com>
2020-03-08 22:32:54 +0900

    Color, Text 수정

--- ElliotableDebug/ViewController.swift
+++ ElliotableDebug/ViewController.swift
@@ -15,14 +15,30 @@ class ViewController: UIViewController {
     private var courseList = [ElliottEvent]()
     private let daySymbol = ["월", "화", "수", "목", "금"]
     
-    @IBOutlet var fullBorderSwitch: UISwitch!
+    private var isAdded = false
+    @IBOutlet weak var lblSwitch: UILabel!
+    @IBOutlet weak var toggleBtn: UIButton!
+    @IBOutlet weak var fullBorderSwitch: UISwitch!
     @IBAction func onClickSwitch(_ sender: UISwitch) {
         elliotable.isFullBorder = sender.isOn
+        lblSwitch.text = sender.isOn ? "테두리 켬" : "테두리 끔"
     }
     
     @IBAction func addItem(_ sender: Any) {
-        courseList.append(ElliottEvent(courseId: "2150000000", courseName: "운제", roomName: "정보과학관\n21204", courseDay: .monday, startTime: "08:00", endTime: "08:55", backgroundColor: UIColor(named: "sample_2")!))
         
+        if !isAdded {
+            courseList.append(ElliottEvent(courseId: "2150000012", courseName: "운제", roomName: "정보과학관\n21204", courseDay: .monday, startTime: "08:00", endTime: "08:55", backgroundColor: UIColor(named: "sample_2")!))
+        
+            courseList.append(ElliottEvent(courseId: "2150000013", courseName: "운제22", roomName: "정보과학관\n21204", courseDay: .thursday, startTime: "20:00", endTime: "21:55", backgroundColor: UIColor(named: "sample_5")!))
+            
+            isAdded = true
+            toggleBtn.setTitle("추가한 항목제거", for: .normal)
+        } else {
+            courseList.removeLast()
+            courseList.removeLast()
+            isAdded = false
+            toggleBtn.setTitle("강의 추가하기", for: .normal)
+        }
         self.elliotable.reloadData()
     }
     
@@ -35,27 +51,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let course_1 = ElliottEvent(courseId: "2150000000", courseName: "운영체제", roomName: "정보과학관\n21204", courseDay: .tuesday, startTime: "12:00", endTime: "13:15", backgroundColor: UIColor(named: "sample_2")!)
+        let course_1 = ElliottEvent(courseId: "2150000001", courseName: "운영체제", roomName: "정보과학관\n21204", courseDay: .tuesday, startTime: "12:00", endTime: "13:15", backgroundColor: UIColor(named: "sample_2")!)
         
-        let course_2 = ElliottEvent(courseId: "2150000000", courseName: "운영체제", roomName: "정보과학관\n21204", courseDay: .thursday, startTime: "12:00", endTime: "13:15", backgroundColor: UIColor(named: "sample_2")!)
+        let course_2 = ElliottEvent(courseId: "2150000002", courseName: "운영체제", roomName: "정보과학관\n21204", courseDay: .thursday, startTime: "12:00", endTime: "13:15", backgroundColor: UIColor(named: "sample_2")!)
         
-        let course_3 = ElliottEvent(courseId: "2150000000", courseName: "빅데이터시스템", roomName: "정보과학관\n21203", courseDay: .tuesday, startTime: "10:30", endTime: "11:45", backgroundColor: UIColor(named: "sample_5")!)
+        let course_3 = ElliottEvent(courseId: "2150000003", courseName: "빅데이터시스템", roomName: "정보과학관\n21203", courseDay: .tuesday, startTime: "10:30", endTime: "11:45", backgroundColor: UIColor(named: "sample_5")!)
         
-        let course_4 = ElliottEvent(courseId: "2150000000", courseName: "정보보안", roomName: "정보과학관\n21201", courseDay: .monday, startTime: "09:00", endTime: "10:15", backgroundColor: UIColor(named: "sample_1")!)
+        let course_4 = ElliottEvent(courseId: "2150000004", courseName: "정보보안", roomName: "정보과학관\n21201", courseDay: .monday, startTime: "09:00", endTime: "10:15", backgroundColor: UIColor(named: "sample_1")!)
         
-        let course_5 = ElliottEvent(courseId: "2150000000", courseName: "정보보안", roomName: "정보과학관\n21201", courseDay: .wednesday, startTime: "09:00", endTime: "10:15", backgroundColor: UIColor(named: "sample_1")!)
+        let course_5 = ElliottEvent(courseId: "2150000005", courseName: "정보보안", roomName: "정보과학관\n21201", courseDay: .wednesday, startTime: "09:00", endTime: "10:15", backgroundColor: UIColor(named: "sample_1")!)
         
-        let course_6 = ElliottEvent(courseId: "2150000000", courseName: "정보기술세미나", roomName: "정보과학관\n21102", courseDay: .thursday, startTime: "16:30", endTime: "17:50", backgroundColor: UIColor(named: "sample_5")!)
+        let course_6 = ElliottEvent(courseId: "2150000006", courseName: "정보기술세미나", roomName: "정보과학관\n21102", courseDay: .thursday, startTime: "16:30", endTime: "17:50", backgroundColor: UIColor(named: "sample_5")!)
         
-        let course_7 = ElliottEvent(courseId: "2150000000", courseName: "컴퓨터공학특강2", roomName: "정보과학관\n21204", courseDay: .tuesday, startTime: "16:30", endTime: "17:45", backgroundColor: UIColor(named: "sample_4")!)
+        let course_7 = ElliottEvent(courseId: "2150000007", courseName: "컴퓨터공학특강2", roomName: "정보과학관\n21204", courseDay: .tuesday, startTime: "16:30", endTime: "17:45", backgroundColor: UIColor(named: "sample_4")!)
         
-        let course_8 = ElliottEvent(courseId: "2150000000", courseName: "컴퓨터공학특강2", roomName: "정보과학관\n21204", courseDay: .friday, startTime: "16:30", endTime: "17:45", backgroundColor: UIColor(named: "sample_4")!)
+        let course_8 = ElliottEvent(courseId: "2150000008", courseName: "컴퓨터공학특강2", roomName: "정보과학관\n21204", courseDay: .friday, startTime: "16:30", endTime: "17:45", backgroundColor: UIColor(named: "sample_4")!)
         
-        let course_9 = ElliottEvent(courseId: "2150000000", courseName: "데이터베이스", roomName: "정보과학관\n21203", courseDay: .monday, startTime: "12:00", endTime: "13:15", backgroundColor: UIColor(named: "sample_3")!)
+        let course_9 = ElliottEvent(courseId: "2150000009", courseName: "데이터베이스", roomName: "정보과학관\n21203", courseDay: .monday, startTime: "12:00", endTime: "13:15", backgroundColor: UIColor(named: "sample_3")!)
         
-        let course_10 = ElliottEvent(courseId: "2150000000", courseName: "데이터베이스", roomName: "정보과학관\n21203", courseDay: .wednesday, startTime: "12:00", endTime: "13:15", backgroundColor: UIColor(named: "sample_3")!)
+        let course_10 = ElliottEvent(courseId: "2150000010", courseName: "데이터베이스", roomName: "정보과학관\n21203", courseDay: .wednesday, startTime: "12:00", endTime: "13:15", backgroundColor: UIColor(named: "sample_3")!)
         
-        let course_11 = ElliottEvent(courseId: "2150000000", courseName: "데이터베이스", roomName: "정보과학관\n21203", courseDay: .wednesday, startTime: "18:00", endTime: "19:45", backgroundColor: UIColor(named: "sample_1")!)
+        let course_11 = ElliottEvent(courseId: "2150000011", courseName: "데이터베이스", roomName: "정보과학관\n21203", courseDay: .wednesday, startTime: "18:00", endTime: "19:45", backgroundColor: UIColor(named: "sample_1")!)
         
         courseList.append(contentsOf: [course_1, course_2, course_3, course_4, course_5, course_6, course_7, course_8, course_9, course_10, course_11
         ])
@@ -72,7 +88,7 @@ class ViewController: UIViewController {
         elliotable.isFullBorder = true
         
         elliotable.borderWidth = 1
-        elliotable.borderColor = UIColor(red: 0.85, green: 0.85, blue: 0.85, alpha: 1.0)
+        elliotable.borderColor = UIColor(hex: "#dadada", alpha: 1.0)
         elliotable.borderCornerRadius = 24
         
         elliotable.textEdgeInsets = UIEdgeInsets(top: 2, left: 5, bottom: 2, right: 3)
@@ -117,5 +133,25 @@ extension ViewController: ElliotableDelegate, ElliotableDataSource {
     
     func elliotable(elliotable: Elliotable, didLongSelectCourse longSelectedCourse : ElliottEvent) {
         showAlert(title: "Long Clicked : \(longSelectedCourse.courseName)", content: "길게 터치되었습니다.")
+    }
+}
+
+extension UIColor {
+    convenience init(hex: String, alpha: CGFloat = 1.0) {
+        var hexFormatted: String = hex.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
+
+        if hexFormatted.hasPrefix("#") {
+            hexFormatted = String(hexFormatted.dropFirst())
+        }
+
+        assert(hexFormatted.count == 6, "Invalid hex code used.")
+
+        var rgbValue: UInt64 = 0
+        Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
+
+        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                  green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                  blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                  alpha: alpha)
     }
 }
